ff530adbbdb6ff835f20409b6547f3ee2ecb1ce8
Iwlj4s <elizavetalenn@mail.ru>
2025-07-31 13:56:50 +0300

    Add pretty svg for update all porojects

--- frontend/src/views/AdminProjects.vue
+++ frontend/src/views/AdminProjects.vue
@@ -7,16 +7,8 @@
         :disabled="isUpdating"
         title="Обновить все проекты"
         >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="logout-icon"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-            :class="{ spin: isUpdating }"
-        >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h5M20 20v-5h-5M5 13a7 7 0 0114-2.5" />
+       <svg class="update-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/>
         </svg>
 
     </button>
